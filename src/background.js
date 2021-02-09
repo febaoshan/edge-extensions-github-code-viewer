6eb554ace16bea48479191517824a9f59476b38b
huangbaoshan <huangbaoshan@cmiot.chinamobile.com>
2021-02-09 18:44:50 +0800

    fix: some bug

--- src/background.js
+++ src/background.js
@@ -1,23 +1,12 @@
-chrome.contextMenus.create({
-    title: 'open with github-code-viewer',
-    contexts: ['page'],
-    onclick: function (params) {
-        const { pageUrl } = params;
-        console.log('pageUrl: ', pageUrl);
-        chrome.tabs.create({ url: pageUrl.replace(/github.com/, 'github1s.com') });
-    },
-    documentUrlPatterns: ['https://*.github.com/*/*']
-});
-
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({ pageUrl: { urlContains: 'github.com' } })
-                ],
-                actions: [new chrome.declarativeContent.ShowPageAction()]
-            }
-        ]);
+if (chrome && chrome.contextMenus) {
+    chrome.contextMenus.create({
+        title: 'open with github-code-viewer',
+        contexts: ['page'],
+        onclick: function (params) {
+            const { pageUrl = '' } = params;
+            
+            chrome.tabs.create({ url: pageUrl.replace(/github.com/, 'github1s.com') });
+        },
+        documentUrlPatterns: ['https://*.github.com/*/*']
     });
-});
+}
